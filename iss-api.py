3b22bf5c0a40c2ce4ff5eda143c3ce51deac14e1
thinkhuman <hijamesgill@gmail.com>
2018-08-27 20:00:47 -0700

    Clean up date and pass data format

--- iss-api.py
+++ iss-api.py
@@ -1,8 +1,12 @@
-# Use the Open Notify open source API from NASA. 
+# Uses the Open Notify open source API from NASA. 
 # check it out: http://open-notify.org/
 
 import requests
+from datetime import datetime
 
+
+# ISS Pass Times
+##############################################
 # params for iss-pass : latitude and longitude
 parameters = {"lat": 45.5214400  , "lon": -122.6769200}
 
@@ -12,14 +16,29 @@ response = requests.get("http://api.open-notify.org/iss-pass.json", params = par
 # jsonify the response.
 iss_data = response.json()
 
-print("ISS Pass Data for your location")
+print("\nISS Pass Data for your location (latitude: {} longitude: {})".format(parameters['lat'], parameters['lon']))
 print("===============================")
 for datapoint in iss_data['response']:
-	print(datapoint)
+    print("Risetime: {}  Duration: {} sec ".format( (datetime.utcfromtimestamp(datapoint['risetime']).strftime('%H:%M:%S')),datapoint['duration']  ))
 
+# ISS Current Location
+###############################################
+
+response = requests.get("http://api.open-notify.org/iss-now.json")
+
+# jsonify the response.
+iss_loc = response.json()
+
+ts = int(iss_loc['timestamp'])
+
+print("\nhe ISS is currently at:")
+print("===============================")
+print("Position: \nLATITUDE: {}  LONGITUDE: {}".format(iss_loc["iss_position"]['latitude'], iss_loc["iss_position"]['longitude']) )
+print("\nDate/Time: \n{}".format(datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')))# People in Space
+###############################################
 # Show the current number of people in space! 
 response = requests.get("http://api.open-notify.org/astros.json")
 
 astros = response.json()
 
-print("\nThere are currently {} astronauts in space.".format(astros["number"]))
+print("\nThere are currently {} astronauts in space.\n".format(astros["number"]))
